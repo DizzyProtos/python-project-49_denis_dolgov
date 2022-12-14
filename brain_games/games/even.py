91db491fd353573c727d8f6da01d2e6e74cc14aa
DenisDolgov1991 <dolgovdenis1991@yandex.ru>
2022-12-14 21:14:52 +0300

    even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,29 +1,27 @@
+#!/usr/bin/env python3
+
 from random import randint
 import prompt
 
 
-def welcome_and_start(game):
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+def even_game():
+    print('Welcome to the Braine Games!')
+    name = promt.string('May I have your name? ')
+    print(f'Hello, {name}')
     print('Answer "yes" if the number is even, otherwise answer "no".')
 
-
-def is_even():
-    round = 3
-    for _ in range(round):
-        x = randint(1, 50)
-        if x % 2 == 0:
+    for _ in range(3):
+        num = randint(1, 50)
+        if num % 2 == 0:
             correct_answer = 'yes'
         else:
             correct_answer = 'no'
-        print(f'Question: {x}')
-        answer = prompt.string('Your answer: ')
+        print('Question:', num)
+        answer = promt.string('Your answer: ')
         if answer == correct_answer:
-            print('Correct!')
+            print('Correct')
         else:
-            print(f"{answer} is wrong answer ;(."
-                  f"Correct answer was {correct_answer}"
-                  f"Let\'s try again, {name}!")
-        break
+            print(f"'{answer}' is wrong answer ;(."
+                  f"Correct answer was '{correct_answer}'."
+                  f"Let's try again, {name}!")
     print(f'Congratulations, {name}!')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -4,7 +4,7 @@ from brain_games.games import even
 
 
 def main():
-    is_even()
+    even_game()
     
 
 if __name__ == '__main__':
