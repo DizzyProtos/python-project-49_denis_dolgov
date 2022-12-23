711dc861fd05c4e3c0a3800f80efae4f04ccb688
dmedvedev <d.medvedev@velvetech.dev>
2022-12-23 23:22:44 +0600

    useless commit

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -6,13 +6,13 @@ from random import randint, choice
 GAME = "What is the result of the expression?"
 
 
-MIN_NUM = 1
+MIIN_NUM = 1
 MAX_NUM = 50
 
 
 def brain_ring():
-    num1 = randint(MIN_NUM, MAX_NUM)
-    num2 = randint(MIN_NUM, MAX_NUM)
+    num1 = randint(MIIN_NUM, MAX_NUM)
+    num2 = randint(MIIN_NUM, MAX_NUM)
     operator = choice('+-*')
     question = f"{num1} {operator} {num2}"
     if operator == '+':
